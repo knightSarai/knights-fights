16f08a3997f6ef7fb8ad757f8fb2d077e4a63d5a
knight <afayyad@celtisaustralis.com>
2022-08-06 22:41:31 +0300

    Move validation pipe and cookie-session into app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe  } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FightsModule } from '../fights/fights.module';
 import { UserModule } from '../user/user.module';
@@ -8,12 +9,14 @@ import { Fight } from '../fights/fight.entity'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const  cookieSession = require('cookie-session');
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: process.env.NODE_ENV === 'test' ? 'test.sqlite': 'db.sqlite',
       entities: [User, Fight],
       synchronize: true,
     }),
@@ -21,6 +24,22 @@ import { AppService } from './app.service';
     UserModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(
+      cookieSession({
+        keys: ['knight']
+      })
+    ).forRoutes('*');
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,25 +3,13 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-const  cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
 
-  app.use(
-    cookieSession({
-      keys: ['knight']
-    })
-  );
-
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
 
   await app.listen(port);
   Logger.log(
